a8256b12870e74f59e2676b455da52d71bf52889
Murilo Beltrame <murilobeltrame@gmail.com>
2022-09-20 22:39:32 -0300

    Initial commit

---
+++ test/wineyard.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Wineyard from '../lib/wineyard-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wineyard-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Wineyard.WineyardStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
